660ca8852694c42dca6349506d68e3efa8b44c5d
dtrinh888 <dtrinh888@gmail.com>
2015-12-12 16:16:31 -0800

    updated the url address

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,16 +1,40 @@
 angular.module('OWMApp', ['ngRoute'])
+	.value('owmCities', ['New York', 'Dallas', 'Chicago'])
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider.when('/', {
 			templateUrl: 'home.html',
 			controller: 'HomeCtrl'
-		}).when('/cities/:city', {
+		}).when('/city', {
 			templateUrl: 'city.html',
-			controller: 'CityCtrl'
+			controller: 'CityCtrl',
+			resolve: {
+				city: function(owmFindCity, $route) {
+					var city = $rotue.current.params.city;
+					return owmFindCity(city);
+				}
+			}
+		})
+		/*.when('/cities/:city', {
+			templateUrl: 'city.html',
+			controller: 'CityCtrl',
+			resolve: {
+				city: function(owmCities, $route, $location) {
+					var city = $route.current.params.city;
+					if(owmCities.indexOf(city) == -1){
+						$location.path('/error');
+						return;
+					}
+					return city;
+				}
+			}
+		})*/
+		.when('/error', {
+			template: '<p>Error - Page Not Found</p>'
 		});
 	}])
 	.controller('HomeCtrl', ['$scope', function($scope){
 
 	}])
-	.controller('CityCtrl', ['$scope', '$routeParams', function($scope, $routeParams){
-		$scope.city = $routeParams.city;
-	}]);
+	.controller('CityCtrl', function($scope, city){
+		$scope.city = city;
+	});
